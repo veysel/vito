14e6b613001530c60ef9fc6e14e04791cfa07e71
veysel <vinanc72@gmail.com>
2017-05-14 12:03:52 +0300

    Home cpp changed and vito file added

--- home.cpp
+++ home.cpp
@@ -1,9 +1,61 @@
 #include <iostream>
+#include <stdlib.h>
+#include <fstream>
 
 using namespace std;
 
+const int ARRAY_COUNT = 100;
+
+void readFile()
+{
+    string toDoList[ARRAY_COUNT][ARRAY_COUNT];
+
+    ifstream fileObject("./ToDoList.vito");
+
+    string tempText;
+    int countFirst = -1;
+    int countSecond = 0;
+    while (fileObject)
+    {
+        fileObject >> tempText;
+        if (tempText == "#")
+        {
+            countFirst++;
+            countSecond = 0;
+        }
+
+        toDoList[countFirst][countSecond] = tempText;
+        countSecond++;
+    }
+
+    int countObject = 0;
+    for (int i = 0; i < ARRAY_COUNT; i++)
+    {
+        for (int j = 0; j < ARRAY_COUNT; j++)
+        {
+            if (toDoList[i][0] != "#")
+            {
+                break;
+            }
+
+            //cout << toDoList[i][j] << " ";
+        }
+
+        if (toDoList[i][0] != "#")
+        {
+            break;
+        }
+
+        countObject++;
+        //cout << endl;
+    }
+
+    cout << endl
+         << "Listenizde Bulunan Not Sayısı : " << countObject << endl;
+}
+
 int main()
 {
-    cout << "Hello Test";
+    readFile();
     cin.get();
 }
